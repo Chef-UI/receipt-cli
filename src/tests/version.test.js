ce1da24837a6d8fb898276b2736de1fca757db8f
cl4pper <cl4pper@gmail.com>
2021-12-22 14:57:13 +0000

    test: default version command

---
+++ src/tests/version.test.js
@@ -0,0 +1,17 @@
+const { system, filesystem } = require('gluegun');
+const pjson = require('../../package.json');
+
+const src = filesystem.path(__dirname, '../..');
+
+const cli = async (cmd) =>
+	system.run('node ' + filesystem.path(src, 'bin', 'receipt-cli') + ` ${cmd}`);
+
+test('should return version for --version', async () => {
+	const output = await cli('--version');
+	expect(output).toContain(pjson.version);
+});
+
+test('should return version for -v', async () => {
+	const output = await cli('-v');
+	expect(output).toContain(pjson.version);
+});
