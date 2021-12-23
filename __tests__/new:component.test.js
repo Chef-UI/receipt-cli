afe7d1503f742579e14e36ce4789100939e0f951
cl4pper <cl4pper@gmail.com>
2021-12-23 23:07:38 +0000

    test: check content from new:component generated files

--- __tests__/new:component.test.js
+++ __tests__/new:component.test.js
@@ -1,9 +1,9 @@
 const { system, filesystem } = require('gluegun');
 
-const src = filesystem.path(__dirname, '..');
+const local = filesystem.path(__dirname, '..');
 
 const cli = async (cmd) =>
-	system.run('node ' + filesystem.path(src, 'bin', 'receipt-cli') + ` ${cmd}`);
+	system.run('node ' + filesystem.path(local, 'bin', 'receipt-cli') + ` ${cmd}`);
 
 const newComponent = 'new:component Topbar';
 const newComponentNamed = 'new:component Footer --named';
@@ -16,24 +16,33 @@ describe('new:component', () => {
 	afterAll(() => filesystem.remove('src/components/'));
 
 	test('should create component file', async () => {
-		const file = filesystem.find('src/components/Topbar', { matching: 'component.jsx' });
+		const src = 'src/components/Topbar';
+		const file = filesystem.find(src, { matching: 'component.jsx' });
+		const content = filesystem.read(`${src}/component.jsx`);
 
 		expect(output).toContain('Topbar component template created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`// Topbar template`);
 	});
 
 	test('should create test file', async () => {
-		const file = filesystem.find('src/components/Topbar', { matching: 'component.test.js' });
+		const src = 'src/components/Topbar';
+		const file = filesystem.find(src, { matching: 'component.test.js' });
+		const content = filesystem.read(`${src}/component.test.js`);
 
 		expect(output).toContain('Topbar component tests created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`// Topbar test`);
 	});
 
 	test('should create style file', async () => {
-		const file = filesystem.find('src/components/Topbar', { matching: 'style.scss' });
+		const src = 'src/components/Topbar';
+		const file = filesystem.find(src, { matching: 'style.scss' });
+		const content = filesystem.read(`${src}/style.scss`);
 
 		expect(output).toContain('Topbar component style created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`.Topbar {`);
 	});
 });
 
@@ -44,23 +53,32 @@ describe('new:component named', () => {
 	afterAll(() => filesystem.remove('src/components/'));
 
 	test('should create Footer.jsx', async () => {
-		const file = filesystem.find('src/components/Footer', { matching: 'Footer.jsx' });
+		const src = 'src/components/Footer';
+		const file = filesystem.find(src, { matching: 'Footer.jsx' });
+		const content = filesystem.read(`${src}/Footer.jsx`);
 
 		expect(output).toContain('Footer component template created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`// Footer template`);
 	});
 
 	test('should create Footer.test.js', async () => {
-		const file = filesystem.find('src/components/Footer', { matching: 'Footer.test.js' });
+		const src = 'src/components/Footer';
+		const file = filesystem.find(src, { matching: 'Footer.test.js' });
+		const content = filesystem.read(`${src}/Footer.test.js`);
 
 		expect(output).toContain('Footer component tests created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`// Footer test`);
 	});
 
 	test('should create style file', async () => {
-		const file = filesystem.find('src/components/Footer', { matching: 'style.scss' });
+		const src = 'src/components/Footer';
+		const file = filesystem.find(src, { matching: 'style.scss' });
+		const content = filesystem.read(`${src}/style.scss`);
 
 		expect(output).toContain('Footer component style created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`.Footer {`);
 	});
 });
