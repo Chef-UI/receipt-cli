8d119278fbc9c6bde89d7cee4d67f5bd4456b53f
cl4pper <cl4pper@gmail.com>
2021-12-24 10:50:15 +0000

    test: check content from new:container generated files

--- __tests__/new:container.test.js
+++ __tests__/new:container.test.js
@@ -8,59 +8,74 @@ const cli = async (cmd) =>
 const newContainer = 'new:container Box';
 const newContainerNamed = 'new:container Modal --named';
 let output;
+let src;
 
 describe('new:container', () => {
 	beforeAll(async () => {
 		output = await cli(newContainer);
+		src = 'src/containers/Box';
 	});
 	afterAll(() => filesystem.remove('src/containers/'));
 
 	test('should create container file', async () => {
-		const file = filesystem.find('src/containers/Box', { matching: 'container.jsx' });
+		const file = filesystem.find(src, { matching: 'container.jsx' });
+		const content = filesystem.read(`${src}/container.jsx`);
 
 		expect(output).toContain('Box container template created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`// Box template`);
 	});
 
 	test('should create test file', async () => {
-		const file = filesystem.find('src/containers/Box', { matching: 'container.test.js' });
+		const file = filesystem.find(src, { matching: 'container.test.js' });
+		const content = filesystem.read(`${src}/container.test.js`);
 
 		expect(output).toContain('Box container tests created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`// Box test`);
 	});
 
 	test('should create style file', async () => {
-		const file = filesystem.find('src/containers/Box', { matching: 'style.scss' });
+		const file = filesystem.find(src, { matching: 'style.scss' });
+		const content = filesystem.read(`${src}/style.scss`);
 
 		expect(output).toContain('Box container style created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`.Box {`);
 	});
 });
 
 describe('new:container named', () => {
 	beforeAll(async () => {
 		output = await cli(newContainerNamed);
+		src = 'src/containers/Modal';
 	});
 	afterAll(() => filesystem.remove('src/containers/'));
 
 	test('should create Modal.jsx', async () => {
 		const file = filesystem.find('src/containers/Modal', { matching: 'Modal.jsx' });
+		const content = filesystem.read(`${src}/Modal.jsx`);
 
 		expect(output).toContain('Modal container template created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`// Modal template`);
 	});
 
 	test('should create Modal.test.js', async () => {
 		const file = filesystem.find('src/containers/Modal', { matching: 'Modal.test.js' });
+		const content = filesystem.read(`${src}/Modal.test.js`);
 
 		expect(output).toContain('Modal container tests created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`// Modal test`);
 	});
 
 	test('should create style file', async () => {
 		const file = filesystem.find('src/containers/Modal', { matching: 'style.scss' });
+		const content = filesystem.read(`${src}/style.scss`);
 
 		expect(output).toContain('Modal container style created.');
 		expect(file.length).toBe(1);
+		expect(content).toContain(`.Modal {`);
 	});
 });
